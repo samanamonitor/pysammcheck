3db6f4582e83313b4faa97aa9c8d4bc1087a74a5
Fabian Baena <fabbaena@gmail.com>
2023-08-10 14:38:38 -0400

    fix base class

--- sammcheck/check.py
+++ sammcheck/check.py
@@ -1,23 +1,59 @@
 import time
 
 class SAMMCheck:
-    def __init__(self, argv=None):
-        self.outmsg = "UNKNOWN - Plugin has not been initialized"
-        self.outval = 3
-        self.ready = False
-        self.done = False
-        self.running = False
-        self.start = 0
-        self.stop = 0
-        if argv is not None:
-            self.process_args(argv)
+    outmsg = "UNKNOWN - Plugin has not been initialized"
+    outval = 3
+    ready = False
+    done = False
+    running = False
+    start = 0
+    stop = 0
+    _crit = None
+    _host = ""
+    _warn = None
+    ready = False
+    _argstring = ""
+    _base_help = '''\
+Check Samana %s v%s
+This nagios plugin come with ABSOLUTELY NO WARRANTY and is property of
+SAMANA GROUP LLC. If you want to use it, you should contact us before
+to get a license.
+Copyright (c) 2021 Samana Group LLC
+
+Usage: %s  [options]
+-h              To get this help message
+'''
+    def __init__(self, argv):
+        self._argv = argv
+        self.process_args()
+        self.check_args()
 
     @property
     def runtime(self):
         return self.stop - self.start
 
-    def process_args(self, argv):
+    def process_args(self):
         pass
+
+    def check_args(self):
+        if self._host == "":
+            self.unknown("Host not specified.")
+            return
+
+        if self._crit is not None:
+            try:
+                self._crit = int(self._crit)
+            except:
+                self.unknown("Invalid CRITICAL threshold.")
+                return
+
+        if self._warn is not None:
+            try:
+                self._warn = int(self._warn)
+            except:
+                self.unknown("Invalid WARNING threshold.")
+                return
+        self.ready = True
 
     def run(self):
         pass

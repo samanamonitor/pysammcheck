1f4ab8f2374c86ef80791de209fb66d501882107
Fabian Baena <fabbaena@gmail.com>
2023-08-10 14:37:12 -0400

    remove class load on init

--- sammcheck/__init__.py
+++ sammcheck/__init__.py
@@ -1,6 +1,6 @@
-from .check import SAMMCheck
-from .etcdcheck import SAMMEtcdCheck
-from .worker import SAMMWorker
+#from .check import SAMMCheck
+#from .etcdcheck import SAMMEtcdCheck
+#from .worker import SAMMWorker
 #from .wmicheck import SAMMWMICheck
 #from .dummycheck import SAMMDummyCheck
 
